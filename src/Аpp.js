3773429b74532e07fd24f27c4c9247020e6c51b6
RichrdYashiro <46631433+RichrdYashiro@users.noreply.github.com>
2025-07-10 12:28:01 +0300

    Started setting
    upt: Started setting product

---
+++ src/Аpp.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const DIV = styled.div`
+	color: red;
+`;
+export const App = () => {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<DIV>DIVnpm</DIV>
+				<i className="fa fa-venus-mars"></i>
+			</header>
+		</div>
+	);
+};
